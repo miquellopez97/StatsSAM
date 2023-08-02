552c3291e8289e840dfe0dde0160b34c288856c1
unknown <miquel.lopez97@gmail.com>
2023-08-03 01:35:24 +0200

    Forts commit
    Components
    TeamRoster
    TeamTemplate
    
    Data
    Demo players

---
+++ stats-sam/src/App.js
@@ -0,0 +1,12 @@
+import TeamTemplate from './components/teamTemplate/TeamTemplate';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Equipo de Baloncesto</h1>
+      <TeamTemplate/>
+    </div>
+  );
+}
+
+export default App;
